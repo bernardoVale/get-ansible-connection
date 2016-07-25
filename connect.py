af3b570b14fc64c2877e0946ed9c3a658ba665ba
bernardoVale <bernardosilveiravale@gmail.com>
2016-07-25 09:24:22 -0300

    Reading ansible cfg to get options

--- connect.py
+++ connect.py
@@ -5,7 +5,21 @@ from ansible import inventory
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars import VariableManager
 from ansible.inventory import Inventory
+from ansible.constants import load_config_file
 
+def load_ssh_args():
+    """
+    Read ansible.cfg and get only the the ssh_args parameter
+    if defined
+    :return: Output of ansible ssh_args
+    """
+    config, _ = load_config_file()
+    # It's easier to ask for forgiveness than permission
+    try:
+        ssh_section = config._sections['ssh_connection']
+        return ssh_section['ssh_args']
+    except KeyError:
+        return None
 
 class SSHInventory:
 
@@ -22,16 +36,24 @@ class SSHInventory:
     def get_ssh_cmd(self, host):
         port = "22"
         user = "root"
-        options = "-F /etc/ansible/ssh.config"
+        options = ""
 
         if 'ansible_port' in host.vars:
             port = host.vars['ansible_port']
+
         if 'ansible_ssh_port' in host.vars:
             port = host.vars['ansible_ssh_port']
+
         if 'ansible_user' in host.vars:
             user = host.vars['ansible_user']
+
         if 'ansible_ssh_user' in host.vars:
             user = host.vars['ansible_ssh_user']
+
+        ssh_args = load_ssh_args()
+        if ssh_args:
+            options += ssh_args
+
         if 'ansible_ssh_private_key_file' in host.vars:
             options += " -i %s" % host.vars['ansible_ssh_private_key_file']
 
@@ -42,6 +64,7 @@ def main(args):
     inv = SSHInventory()
     hosts = inv.inventory.get_hosts(pattern)
     for host in hosts:
+        print "Server:%s" % host.name
         print inv.get_ssh_cmd(host)
 
 if __name__ == '__main__':
